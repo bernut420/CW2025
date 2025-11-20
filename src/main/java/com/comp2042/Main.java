71a9971b14f9fd73132d397679eefc942f03ee18
bernut420 <131754604+bernut420@users.noreply.github.com>
2025-11-21 00:29:01 +0800

    Update application configuration
    -Setup fullscreen support
    -Configure JavaFX module path and dependencies

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -13,19 +14,29 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        URL location = getClass().getClassLoader().getResource("gameLayout.fxml");
-        ResourceBundle resources = null;
-        FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gameLayout.fxml"));
         Parent root = fxmlLoader.load();
         GuiController c = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 300, 510);
+        Scene scene = new Scene(root, 400, 600); // Slightly larger initial size
+
+        // Enable fullscreen support
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(500);
+        primaryStage.setFullScreenExitHint("Press ESC to exit fullscreen");
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("ESC"));
+
         primaryStage.setScene(scene);
         primaryStage.show();
         new GameController(c);
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
 
 
     public static void main(String[] args) {
